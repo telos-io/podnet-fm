bb266ac8181cffdaac5a55a20e33fcf1b481941c
KoanBrown <koan.brown@gmail.com>
2016-01-12 17:42:19 -0800

    changed devise model and sign up

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,18 @@
 class WelcomeController < ApplicationController
 
-  def index
-  end
-
-  def podcasts
-  end
-
-  def show
-  end
-
-  def episode
-  end
-
-  def dashboard
-  end
+  # def index
+  # end
+  #
+  # def podcasts
+  # end
+  #
+  # def show
+  # end
+  #
+  # def episode
+  # end
+  #
+  # def dashboard
+  # end
 
 end
